a67a1ec0f9fb878183315807d1cd508abf36b875
Abhishek Rana <abhishekrana502@gmail.com>
2017-06-28 14:51:53 +0200

    Tensorflow CPU support added

---
+++ dockerfiles/Dockerfile_cpu_tf1.1
@@ -0,0 +1,29 @@
+FROM gcr.io/tensorflow/tensorflow:1.1.0
+
+ENV HOME /home/user
+
+# Install dependencies
+RUN apt-get update && apt-get install -y \
+	git \
+    wget \
+	python-tk \
+	locate \
+	ctags \
+	vim \
+	tree \
+	protobuf-compiler \
+	python-pil \
+	python-lxml
+
+RUN pip install \
+	future \
+	Cython \
+	opencv-python \
+	scikit-image \
+	keras
+
+
+RUN mkdir -p /home/user/
+
+WORKDIR /home/user
+
--- install.py
+++ install.py
@@ -7,67 +7,82 @@ import os
 
 ### FUNCTIONS ###
 
-def setup_docker_image(doc_repo_name, docker_file):
+# Installs/updated Docker CE
+def install_docker():
+
+	print('Installing/Updating Docker')
+
+	cmd_host('sudo apt-get remove docker docker-engine')
+
+	cmd_host('sudo apt-get update')
+
+	# Recommended extra packages for Trusty 14.04
+	#cmd_host('sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual')
+
+	# Install packages to allow apt to use a repository over HTTPS
+	cmd_host('sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common')
+
+	# Add Docker’s official GPG key
+	cmd_host('curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -')
+
+	# Set up the stable repository
+	cmd_host('sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"')
+
+	# Install/Update docker
+	cmd_host('sudo apt-get update')
+	cmd_host('sudo apt-cache policy docker-ce')
+	cmd_host('sudo apt-get -y install docker-ce')
+	#sudo apt-get -y install docker-ce=<VERSION>
+
+	# Test installation
+	cmd_host('sudo docker run hello-world')
+
+
+def setup_docker_image(doc_repo_name, docker_file, gpu):
     print('### setup_docker_image ###')
 
-    # Run non-GPU container using
-    # subprocess.call('sudo docker pull gcr.io/tensorflow/tensorflow', shell=True)
+    if gpu is 0:
+        # Run non-GPU container using
+        cmd_host('sudo docker pull gcr.io/tensorflow/tensorflow')
 
-
-    # Install nvidia-docker and nvidia-docker-plugin
-    subprocess.call('wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb', shell=True)
-    subprocess.call('sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb', shell=True)
-    subprocess.call('sudo nvidia-docker run --rm nvidia/cuda nvidia-smi', shell=True)
-
+    if gpu is 1:
+        # Install nvidia-docker and nvidia-docker-plugin
+        cmd_host('wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb')
+        cmd_host('sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb')
+        cmd_host('sudo nvidia-docker run --rm nvidia/cuda nvidia-smi')
 
     # Create Docker Image
-    cmd='sudo ' + DOCKER + ' rm -f ' + container_name
-    print('cmd', cmd)
-    subprocess.call(cmd, shell=True)
+    cmd_host('sudo ' + DOCKER + ' rm -f ' + container_name)
 
-    cmd='sudo ' + DOCKER + ' rmi ' +  doc_repo_name
-    print('cmd', cmd)
-    subprocess.call(cmd , shell=True)
+    cmd_host('sudo ' + DOCKER + ' rmi ' +  doc_repo_name)
 
     print("Generating Docker image using $docker_file ")
-    cmd='sudo ' + DOCKER + ' build -f ' + docker_file + ' -t ' + doc_repo_name + ' .'
-    print('cmd', cmd)
-    subprocess.call(cmd, shell=True)
+    cmd_host('sudo ' + DOCKER + ' build -f ' + docker_file + ' -t ' + doc_repo_name + ' .')
 
 
 
 def setup_docker_container(doc_repo_name, container_name, host_project_path, docker_project_path):
     print('### setup_docker_container ###')
 
-    cmd='sudo ' + DOCKER + ' rm -f ' + container_name
-    print('cmd', cmd)
-    subprocess.call(cmd, shell=True)
+    cmd_host('sudo ' + DOCKER + ' rm -f ' + container_name)
+    cmd_host('mkdir -p ' + host_project_path)
 
-    cmd='mkdir -p ' + host_project_path
-    print('cmd', cmd)
-    subprocess.call(cmd, shell=True)
-
-    cmd='sudo ' + DOCKER + ' run -d -v ' + host_project_path + ':' + docker_project_path + ' --name ' + container_name + ' ' + doc_repo_name
-    print('cmd', cmd)
-    subprocess.call(cmd, shell=True)
+    cmd_host('sudo ' + DOCKER + ' run -d -v ' + host_project_path + ':' + docker_project_path + ' --name ' + container_name + ' ' + doc_repo_name)
 
 
 def setup_dot_files(container_name, user):
+    print('### setup_dot_files ###')
 
     cmd_cont='git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim'
     cmd_container(container_name, cmd_cont)
 
-    cmd='sudo ' + DOCKER + ' cp dotfiles/vim/.vimrc ' + container_name + ':/home/' + user
-    print('cmd', cmd)
-    subprocess.call(cmd, shell=True)
+    cmd_host('sudo ' + DOCKER + ' cp dotfiles/vim/.vimrc ' + container_name + ':/home/' + user)
 
     cmd_cont='vim +BundleInstall +qall'
     cmd_container(container_name, cmd_cont)
 
 
-
-
-# Creating directories outside container due to 'permission denied' issue
+# Creating directories outside container due to 'permission denied' issue (as docker user is root)
 def setup_maskrcnn(container_name, project_name, repo_link, repo_name, resnet50_dataset_path=None, coco_dataset_path=None, delete_old_repo=None):
     print('### setup_maskrcnn ###')
 
@@ -77,12 +92,10 @@ def setup_maskrcnn(container_name, project_name, repo_link, repo_name, resnet50_
     if delete_old_repo is 'y':
         cmd_cont='rm -rf ' + project_name + '/*'
         cmd_container(container_name, cmd_cont)
-        #cmd_host(cmd_cont)
 
 
     # Don't remove project_name directory as it is mounted inside docker
     cmd_cont='git clone ' + repo_link + ' ' + project_name + '/' + repo_name
-    #cmd_container(container_name, cmd_cont)
     cmd_host(cmd_cont)
 
 
@@ -91,7 +104,6 @@ def setup_maskrcnn(container_name, project_name, repo_link, repo_name, resnet50_
     cmd_container(container_name, cmd_cont)
 
     cmd_cont=cd_root_dir + 'mkdir -p data/coco; mkdir -p data/pretrained_models; mkdir -p output/mask_rcnn'
-    # cmd_container(container_name, cmd_cont)
     cmd_host(cmd_cont)
 
 
@@ -152,14 +164,14 @@ def train_maskrcnn(container_name, project_name, repo_name):
 ### HELPER FUNCTIONS
 
 def cmd_host(cmd):
-    print('cmd: ', cmd)
+    print('$ cmd: ', cmd)
     subprocess.call(cmd, shell=True)
 
 def cmd_container(container_name, cmd_cont):
 
-    print('cmd_cont : ', cmd_cont)
+    print('$ cmd_cont : ', cmd_cont)
     cmd='sudo '+ DOCKER + ' exec -it ' + container_name + ' bash -c "' + cmd_cont + '"'
-    print('cmd      : ', cmd)
+    print('$ cmd      : ', cmd)
     subprocess.call(cmd, shell=True)
 
 
@@ -167,7 +179,16 @@ def cmd_container(container_name, cmd_cont):
 
 
 ### GLOBALS
-DOCKER="nvidia-docker"
+
+# CPU
+#gpu=0
+#DOCKER="nvidia"
+#docker_file         = 'dockerfiles/Dockerfile_cpu_tf1.1'
+
+# GPU
+gpu=1
+DOCKER              = "nvidia-docker"
+docker_file         = 'dockerfiles/Dockerfile_gpu_tf1.1'
 
 # Also modify in .dockerignore
 project_name='MRCNN'
@@ -176,14 +197,13 @@ dataset_name='Dataset'
 # Also modify in Dockerfile
 user='user'
 
-docker_file         = 'dockerfiles/Dockerfile_gpu_tf1.1'
 tf_version          = docker_file.split("_")
-doc_repo_name           = user+'/tensorflow_gpu_'+project_name.lower()
+doc_repo_name       = user+'/tensorflow_gpu_'+project_name.lower()
 container_name      = user+'-'+project_name.lower()+'_'+tf_version[-1]
 host_project_path   = os.path.join(os.getcwd(), project_name)
 docker_project_path = os.path.join('/home/'+user+'/', project_name)
 repo_link           = 'https://github.com/CharlesShang/FastMaskRCNN'
-repo_name           =  repo_link.split("/")[-1]
+repo_name           = repo_link.split("/")[-1]
 
 
 ### USER INPUT
@@ -194,17 +214,18 @@ resnet50_dataset_path = input('Enter resnet_v1_50_2016_08_28.tar.gz ABSOLUTE PAT
 print('Resnet50 dataset path: ', resnet50_dataset_path)
 
 
-coco_dataset_path = input('Enter train2014.zip, val2014.zip, instances_train-val2014.zip, person_keypoints_trainval2014.zip and \ncaptions_train-val2014.zip ABSOLUTE PATH starting with / [Example: /home/Downloads] (or Press Enter to download): ')
+coco_dataset_path = input('\nEnter train2014.zip, val2014.zip, instances_train-val2014.zip, person_keypoints_trainval2014.zip and \ncaptions_train-val2014.zip ABSOLUTE PATH starting with / [Example: /home/Downloads] (or Press Enter to download): ')
 print('COCO dataset path: ', coco_dataset_path)
 
-delete_old_repo = input('Delete old repository (' + os.getcwd() + '/' +  project_name + '/' + repo_name + '): ')
+delete_old_repo = input('\nDelete old repository (' + os.getcwd() + '/' +  project_name + '/' + repo_name + ') [y/n]: ')
 print('Delete: ', coco_dataset_path)
 
 
 ### FUNCTIONS
 
 # DOCKER
-setup_docker_image(doc_repo_name, docker_file)
+#install_docker()
+setup_docker_image(doc_repo_name, docker_file, gpu)
 setup_docker_container(doc_repo_name, container_name, host_project_path, docker_project_path)
 setup_dot_files(container_name, user)
 
@@ -218,15 +239,23 @@ train_maskrcnn(container_name, project_name, repo_name)
 print('\n##################################################')
 print('Project name         : ', project_name)
 print('Docker file          : ', docker_file)
-print('Repo name            : ', doc_repo_name)
+print('Docker repo name     : ', doc_repo_name)
 print('Container name       : ', container_name)
 print('Host project path    : ', host_project_path)
 print('Docker project path  : ', docker_project_path)
 print('Repository           : ', repo_link)
 print('Repository Name      : ', repo_name)
+print('GPU                  : ', gpu)
 
-print('\nCommands:')
-print('sudo ' + DOCKER + ' exec -it ' + container_name + ' bash \n')
-print('##################################################\n')
+if gpu is 1:
+    print('\nTo disable GPU (inside docker): \nexport CUDA_VISIBLE_DEVICES=')
+
+print('\nTo access docker:')
+print('sudo ' + DOCKER + ' exec -it ' + container_name + ' bash ')
+
+print('\nTo train:')
+print('sudo nvidia-docker exec -it user-mrcnn_tf1.1 bash -c "cd MRCNN/FastMaskRCNN; python train/train.py" \n')
+
+print('##################################################')
 
 
